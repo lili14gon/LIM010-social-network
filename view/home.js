288dd2c1617bc5284eff63569d3f7d470bfb7ea5
Marilia Yovana <marilia_yovana@hotmail.com>
2019-08-13 00:42:12 -0500

    textarea

--- view/home.js
+++ view/home.js
@@ -25,8 +25,9 @@ export const screenHome = () => {
       </div>
     </div>
     <div class="colunm-post">
-    <textarea  class="text"name="textarea" rows="5" cols="70">¿Que quieres compartir?</textarea>
-    <button id="compartir">Compartir</button>
+    <p><textarea class="estilotextarea"name="comentarios" required  placeholder="¿Que quieres compartir?"></textarea>
+    </p>
+    <p><input type="submit" value="compartir"><p>
     </div>
   </div>`;
 
